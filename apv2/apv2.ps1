0a8d5312a7d15052fa16d8356e2f3277429146fd
Armando Negron <137515948+armandonegron@users.noreply.github.com>
2024-10-29 15:18:31 -0400

    Add files via upload

---
+++ apv2/apv2.ps1
@@ -0,0 +1,34 @@
+﻿$spTenant = "leegovfl.sharepoint.com"
+$spSitePath = "/sites/InformationTechnology"
+$spLibrary = "apv2"
+$outputFolder = "c:\ITS"
+
+Connect-MgGraph
+
+$sps = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$($spTenant):/$($spSitePath)"
+
+$spds = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$($sps.id)/drives?$filter=name eq 'apv2'"
+
+$driveId = ""
+
+foreach ($spd in $spds.value)
+{
+    if($spd.name -eq $spLibrary) {
+        $driveId = $spd.id
+    }
+}
+$driveId
+
+#create dir with permissons?
+
+if($driveId -ne ""){
+
+    $dis = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/drives/$($driveid)/root/children"
+
+    foreach ($di in $dis.value)
+    {
+        Get-MgDriveItemContent -DriveId $driveId -DriveItemId $di.id -OutFile "$($outputFolder)\$($di.name)"
+    }
+
+
+}
