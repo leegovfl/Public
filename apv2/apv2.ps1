987903c313732d11e53116a8372c591e62072d1f
Armando Negron <137515948+armandonegron@users.noreply.github.com>
2024-11-10 12:45:26 -0500

    Update apv2.ps1

--- apv2/apv2.ps1
+++ apv2/apv2.ps1
@@ -11,10 +11,26 @@ Import-Module Microsoft.Graph.Files
 
 Connect-MgGraph
 
+# Create the folder
+New-Item -Path $outputFolder -ItemType Directory
+
+# Set permissions to allow only Administrators full access
+$acl = Get-Acl $outputFolder
+$acl.SetAccessRuleProtection($true, $false)
+
+# Remove inherited permissions
+$acl.SetAccessRuleProtection($true, $false)
+
+# Add full control for Administrators
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "Allow")
+$acl.AddAccessRule($rule)
+
+# Set the ACL on the folder
+Set-Acl $outputFolder $acl
+
+
 $sps = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$($spTenant):/$($spSitePath)"
-
 $spds = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$($sps.id)/drives?$filter=name eq 'apv2'"
-
 $driveId = ""
 
 foreach ($spd in $spds.value)
