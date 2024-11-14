815f248a26a6866aa1f211c9cec802d1cb2ceb71
Armando Negron <137515948+armandonegron@users.noreply.github.com>
2024-11-14 15:40:30 -0500

    Update apv2.ps1

--- apv2/apv2.ps1
+++ apv2/apv2.ps1
@@ -70,7 +70,8 @@ if ($decision -eq 0) {
     powershell.exe -executionpolicy bypass -file "$($outputFolder)\pro2ent.ps1"
     powershell.exe -executionpolicy bypass -file "$($outputFolder)\add2apv2.ps1"
     powershell.exe -executionpolicy bypass -file "$($outputFolder)\drivemappingscheduler.ps1"    
-    #powershell.exe -executionpolicy bypass -file "$($outputFolder)\addBackgrounds.ps1"
+    powershell.exe -executionpolicy bypass -file "$($outputFolder)\addBackgrounds.ps1"
+    powershell.exe -executionpolicy bypass -file "$($outputFolder)\sysprep.ps1"        
     #powershell.exe -executionpolicy bypass -file "$($outputFolder)\renamePC.ps1"
     
     #Disable-LocalUser -Name "Administrator"
