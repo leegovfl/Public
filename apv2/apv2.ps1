3d3277163f4996c2c01a8ce73e0681dfcfd623b4
Armando Negron <137515948+armandonegron@users.noreply.github.com>
2025-01-03 10:29:45 -0500

    Update apv2.ps1

--- apv2/apv2.ps1
+++ apv2/apv2.ps1
@@ -89,8 +89,10 @@ if ($build.CurrentBuildNumber -ge 22631){
         powershell.exe -executionpolicy bypass -file "$($outputFolder)\drivemappingscheduler.ps1"    
         powershell.exe -executionpolicy bypass -file "$($outputFolder)\addBackgrounds.ps1"
         powershell.exe -executionpolicy bypass -file "$($outputFolder)\settings.ps1"
+        cd "$($env:SystemRoot)\system32\sysprep"
+        .\sysprep.exe /unattend:"c:\its\Unattend.xml" /oobe #/reboot
         powershell.exe -executionpolicy bypass -file "$($outputFolder)\renamePC.ps1"
-        powershell.exe -executionpolicy bypass -file "$($outputFolder)\sysprep.ps1"
+        #powershell.exe -executionpolicy bypass -file "$($outputFolder)\sysprep.ps1"
         
         $title    = 'Restart Computer'
         $question = 'Do you want to restart this computer (recommended)?'
