4c2a1e1d642c2989393db9e68a5e964813d418bd
Armando Negron <137515948+armandonegron@users.noreply.github.com>
2025-01-28 09:32:38 -0500

    Update apv1.ps1

--- apv2/apv1.ps1
+++ apv2/apv1.ps1
@@ -4,7 +4,7 @@ param (
 )
 $hottogo = $false
 
-if (Test-Path -Path "$($env:ProgramData)\LeeCounty\PreProvision\PreProvision.tag") {    
+if (Test-Path -Path "$($env:ProgramData)\LeeCounty\PreProvision\PreProvisionV1.tag") {    
     $title    = 'Re-run Script'
     $question = 'This script has already been ran on this device, do you want to re-run it?'
     $choices  = '&Yes', '&No', '&Repeat the Question'
@@ -22,8 +22,8 @@ if($hottogo){
     $build = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion") | Select-Object -Property DisplayVersion,CurrentBuildNumber,UBR
     write-host "Current Windows Version: $($build.DisplayVersion) $($build.CurrentBuildNumber).$($build.UBR)" -ForegroundColor Cyan
     #min required: 22H2 22621.3374 or 23H2 22631.3374 or 24H2
-    if (($build.CurrentBuildNumber -eq 22621 -and $build.UBR -ge 3374) -or ($build.CurrentBuildNumber -eq 22631 -and $build.UBR -ge 3374) -or $build.CurrentBuildNumber -ge 26100)
-    {
+    #if (($build.CurrentBuildNumber -eq 22621 -and $build.UBR -ge 3374) -or ($build.CurrentBuildNumber -eq 22631 -and $build.UBR -ge 3374) -or $build.CurrentBuildNumber -ge 26100)
+    #{
         $spTenant = "leegovfl.sharepoint.com"
         $spSitePath = "/sites/InformationTechnology"
         $spLibrary = "apv2"
@@ -111,11 +111,11 @@ if($hottogo){
             #provision computer
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\registerDevice.ps1"
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\pro2ent.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\add2apv2.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\drivemappingscheduler.ps1"    
+            powershell.exe -executionpolicy bypass -file "$($outputFolder)\add2apv1.ps1"
+            #powershell.exe -executionpolicy bypass -file "$($outputFolder)\drivemappingscheduler.ps1"    
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\addBackgrounds.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\settings.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\renamePC.ps1"
+            powershell.exe -executionpolicy bypass -file "$($outputFolder)\settingsV1.ps1"
+            #powershell.exe -executionpolicy bypass -file "$($outputFolder)\renamePC.ps1"
 
             #write installed tag
             # Create a tag file just so Intune knows this was installed
@@ -123,7 +123,7 @@ if($hottogo){
             {
                 Mkdir "$($env:ProgramData)\LeeCounty\PreProvision"
             }
-            Set-Content -Path "$($env:ProgramData)\LeeCounty\PreProvision\PreProvision.tag" -Value "Installed"
+            Set-Content -Path "$($env:ProgramData)\LeeCounty\PreProvision\PreProvisionV1.tag" -Value "Installed"
             
             $title    = 'Restart Computer'
             $question = 'Do you want to restart this computer now (recommended)?'
@@ -137,21 +137,21 @@ if($hottogo){
             }
         }
     
-    }else {
-    
-        write-host "This version of Windows is not compatible with Autopilot V2. Please update to at least Windows 11 22H2 22621.3374 or 23H2 22631.3374 or 24H2"
-        $title    = 'Update Windows'
-        $question = 'Do you want to run Winndows Update?'
-        $choices  = '&Yes', '&No', '&Repeat the Question'
-        do {
-            $decisionW = $Host.UI.PromptForChoice($title, $question, $choices, 2)
-        } while ($decisionW -ne 0 -and $decisionW -ne 1)
-        if ($decisionW -eq 0) {    
-            Install-Module -Name PSWindowsUpdate -Force
-            Import-Module -Name PSWindowsUpdate
-            Get-WindowsUpdate -AcceptAll -Install -AutoReboot
-        }
+    #} else {
+    #
+    #    write-host "This version of Windows is not compatible with Autopilot V2. Please update to at least Windows 11 22H2 22621.3374 or 23H2 22631.3374 or 24H2"
+    #    $title    = 'Update Windows'
+    #    $question = 'Do you want to run Winndows Update?'
+    #    $choices  = '&Yes', '&No', '&Repeat the Question'
+    #    do {
+    #        $decisionW = $Host.UI.PromptForChoice($title, $question, $choices, 2)
+    #    } while ($decisionW -ne 0 -and $decisionW -ne 1)
+    #    if ($decisionW -eq 0) {    
+    #        Install-Module -Name PSWindowsUpdate -Force
+    #        Import-Module -Name PSWindowsUpdate
+    #        Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+    #    }
 
         
-    }
+    #}
 }
