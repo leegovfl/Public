f97b5961fc8886628631693e14113651343d3e20
Armando Negron <137515948+armandonegron@users.noreply.github.com>
2025-01-31 13:17:36 -0500

    Update apv2.ps1

--- apv2/apv2.ps1
+++ apv2/apv2.ps1
@@ -113,7 +113,7 @@ if($hottogo){
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\pro2ent.ps1"
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\add2apv2.ps1"
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\drivemappingscheduler.ps1"            
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\addBackgroundsThemes.ps1"
+            powershell.exe -executionpolicy bypass -file "$($outputFolder)\addBackgrounds.ps1"
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\settings.ps1"
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\renamePC.ps1"
 
