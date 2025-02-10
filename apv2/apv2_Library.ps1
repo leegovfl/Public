39c73eb37a7e6c77951f159cb9a1703654119ee6
Armando Negron <137515948+armandonegron@users.noreply.github.com>
2025-02-10 11:34:04 -0500

    Update apv2_Library.ps1

--- apv2/apv2_Library.ps1
+++ apv2/apv2_Library.ps1
@@ -116,11 +116,11 @@ if($hottogo){
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\addBackground.ps1"
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\settings.ps1"
             powershell.exe -executionpolicy bypass -file "$($outputFolder)\renamePC.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\InstallBrowsers.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\desktopShortcuts.ps1"
+            #powershell.exe -executionpolicy bypass -file "$($outputFolder)\InstallBrowsers.ps1"
+            #powershell.exe -executionpolicy bypass -file "$($outputFolder)\desktopShortcuts.ps1"
             #powershell.exe -executionpolicy bypass -file "$($outputFolder)\Configure_Autologon_FirstLogon_Task.ps1"
             #powershell.exe -executionpolicy bypass -file "$($outputFolder)\Configure_UWF_FirstLogon_Task.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\Configure_UWF_Tasks.ps1"
+            #powershell.exe -executionpolicy bypass -file "$($outputFolder)\Configure_UWF_Tasks.ps1"
 
             #write installed tag
             # Create a tag file just so Intune knows this was installed
