0e382d1b4a30f8ccc9de546fa84d62d67294e091
Armando Negron <137515948+armandonegron@users.noreply.github.com>
2025-02-24 11:32:28 -0500

    Update Post-APV2-Reboot.ps1

--- apv2/Post-APV2-Reboot.ps1
+++ apv2/Post-APV2-Reboot.ps1
@@ -6,12 +6,12 @@ if (-not (Test-Path $dest))
 Start-Transcript "$dest\Post-APV2-Reboot.log" #-Append
 
 
-Start-ScheduledTask -TaskName "Post-APV2-Notification"
+Start-ScheduledTask -TaskName "Post-APV2-Reboot-Notification"
 Start-Sleep -Seconds 21
 
 disable-scheduledtask -taskname Post-APV2-Reboot -ErrorAction SilentlyContinue -Verbose
 #Unregister-ScheduledTask -TaskName PostESP-Reboot -Confirm:$false
-disable-scheduledtask -taskname Post-APV2-Notification -Erroraction Continue -Verbose
+disable-scheduledtask -taskname Post-APV2-Reboot-Notification -Erroraction Continue -Verbose
 #Unregister-ScheduledTask -TaskName PostESP-Reboot-Notification -Confirm:$false
 
 
