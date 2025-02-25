12d93498a9d4a77cf125e061e45c1c8493c02e3f
Armando Negron <137515948+armandonegron@users.noreply.github.com>
2025-02-25 09:20:56 -0500

    Update apv2.ps1

--- apv2/apv2.ps1
+++ apv2/apv2.ps1
@@ -31,127 +31,136 @@ if($hottogo){
         
         if(-not $F)
         {
+            write-host "Installing Libraries" -ForegroundColor Yellow
             Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
             Install-Module Microsoft.Graph -Force
             #Import-Module Microsoft.Graph
             #Install-Module Microsoft.Graph.Files -Force
             Import-Module Microsoft.Graph.Files
         }
-        Connect-MgGraph -Scopes "Device.ReadWrite.All,DeviceManagementManagedDevices.ReadWrite.All,DeviceManagementServiceConfig.ReadWrite.All,Sites.ReadWrite.All,User.Read.All"
-        
-        # Create the folder
-        if (-not (Test-Path $outputFolder))
-        {
-            New-Item -Path $outputFolder -ItemType Directory
-        }
-        
-        # Set permissions to allow only Administrators full access
-        $acl = Get-Acl $outputFolder
-        $acl.SetAccessRuleProtection($true, $false)
-        
-        # Remove inherited permissions
-        $acl.SetAccessRuleProtection($true, $false)
-        
-        # Add full control for Administrators
-        $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "Allow")
-        $acl.AddAccessRule($rule)
-        
-        # Set the ACL on the folder
-        Set-Acl $outputFolder $acl
-        
-        
-        $sps = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$($spTenant):/$($spSitePath)"
-        $spds = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$($sps.id)/drives?$filter=name eq '$($spLibrary)'"
-        $driveId = ""
-        
-        foreach ($spd in $spds.value)
-        {
-            if($spd.name -eq $spLibrary) {
-                $driveId = $spd.id
-            }
-        }
-        
-        if($driveId -ne ""){
-        
-            $dis = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/drives/$($driveid)/root/children"
-        
-            foreach ($di in $dis.value)
-            {
-                Get-MgDriveItemContent -DriveId $driveId -DriveItemId $di.id -OutFile "$($outputFolder)\$($di.name)"
-            }
-        
-        
-        }
-        #copy drivemappings file
-        $dest = "$($env:ProgramData)\LeeCounty\DriveMapping"
-        if (-not (Test-Path $dest))
-        {
-            mkdir $dest
-        }
-        Copy-Item "$($outputFolder)\DriveMappings.ps1" -Destination $dest -Force  
-        
-        #$dest = "$($env:ProgramData)\LeeCounty\renamePC"
-        #if (-not (Test-Path $dest))
-        #{
-        #    mkdir $dest
-        #}
-        #Copy-Item "$($outputFolder)\Post-APV2-Reboot.ps1" -Destination $dest -Force  
-        #Copy-Item "$($outputFolder)\Post-APV2-Reboot-Notification.ps1" -Destination $dest -Force  
-        #copy oobe.xml
-        #$dest = "$($env:windir)\System32\Oobe\Info"
-        #if (-not (Test-Path $dest))
-        #{
-        #    mkdir $dest
-        #}
-        #Copy-Item "$($outputFolder)\oobe.xml" -Destination $dest -Force  
-        
         $decision = 0
-        if(-not $P)
+        $title    = 'Download Files'
+        $question = 'Do you want to start downloading files on this computer?'
+        if(-not $F)
         {
             $title    = 'Pre-Provision Computer'
             $question = 'Do you want to start the pre-provisioning process on this computer?'
+        }
+        if(-not $P)
+        {
+
             $choices  = '&Yes', '&No', '&Repeat the Question'
             do {
                 $decision = $Host.UI.PromptForChoice($title, $question, $choices, 2)
             } while ($decision -ne 0 -and $decision -ne 1)
         }
         if ($decision -eq 0 -or $P) {
-            #provision computer
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\registerDevice.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\pro2ent.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\add2apv2.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\drivemappingscheduler.ps1"            
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\addBackgrounds.ps1"
-            powershell.exe -executionpolicy bypass -file "$($outputFolder)\settings.ps1"
-            #powershell.exe -executionpolicy bypass -file "$($outputFolder)\renamePC.ps1"
-
-            #write installed tag
-            # Create a tag file just so Intune knows this was installed
-            if (-not (Test-Path "$($env:ProgramData)\LeeCounty\PreProvision"))
-            {
-                Mkdir "$($env:ProgramData)\LeeCounty\PreProvision"
-            }
-            Set-Content -Path "$($env:ProgramData)\LeeCounty\PreProvision\PreProvision.tag" -Value "Installed"
+            Connect-MgGraph -Scopes "Device.ReadWrite.All,DeviceManagementManagedDevices.ReadWrite.All,DeviceManagementServiceConfig.ReadWrite.All,Sites.ReadWrite.All,User.Read.All"
             
-            Write-Host "*******************************************************************************************" -ForegroundColor Cyan
-            Write-Host "* Pre-Provisioning Complete. Close this window to continue provisioning this device." -ForegroundColor Cyan
-            Write-Host "*******************************************************************************************" -ForegroundColor Cyan
-
-            #exit 1641
-            #$title    = 'Restart Computer'
-            #$question = 'Do you want to restart this computer now (recommended)?'
-            #$title    = 'Provision Computer'
-            #$question = 'Do you want to continue provisioning this computer?'
-            #$choices  = '&Yes', '&No', '&Repeat the Question'
-            #do {
-            #    $decisionR = $Host.UI.PromptForChoice($title, $question, $choices, 2)
-            #} while ($decisionR -ne 0 -and $decisionR -ne 1)
-            #if ($decisionR -eq 0) {    
-                #powershell.exe -executionpolicy bypass -file "$($outputFolder)\sysprep.ps1"
-                #shutdown /g /f /t 0
-                #exit 1641
-                #exit
+            # Create the folder
+            if (-not (Test-Path $outputFolder))
+            {
+                New-Item -Path $outputFolder -ItemType Directory
+            }
+            
+            # Set permissions to allow only Administrators full access
+            $acl = Get-Acl $outputFolder
+            $acl.SetAccessRuleProtection($true, $false)
+            
+            # Remove inherited permissions
+            $acl.SetAccessRuleProtection($true, $false)
+            
+            # Add full control for Administrators
+            $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "Allow")
+            $acl.AddAccessRule($rule)
+            
+            # Set the ACL on the folder
+            Set-Acl $outputFolder $acl
+            
+            
+            $sps = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$($spTenant):/$($spSitePath)"
+            $spds = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$($sps.id)/drives?$filter=name eq '$($spLibrary)'"
+            $driveId = ""
+            
+            foreach ($spd in $spds.value)
+            {
+                if($spd.name -eq $spLibrary) {
+                    $driveId = $spd.id
+                }
+            }
+            
+            if($driveId -ne ""){
+            
+                $dis = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/drives/$($driveid)/root/children"
+            
+                foreach ($di in $dis.value)
+                {
+                    Get-MgDriveItemContent -DriveId $driveId -DriveItemId $di.id -OutFile "$($outputFolder)\$($di.name)"
+                }
+            
+            
+            }
+            #copy drivemappings file
+            $dest = "$($env:ProgramData)\LeeCounty\DriveMapping"
+            if (-not (Test-Path $dest))
+            {
+                mkdir $dest
+            }
+            Copy-Item "$($outputFolder)\DriveMappings.ps1" -Destination $dest -Force  
+        
+            #$dest = "$($env:ProgramData)\LeeCounty\renamePC"
+            #if (-not (Test-Path $dest))
+            #{
+            #    mkdir $dest
             #}
+            #Copy-Item "$($outputFolder)\Post-APV2-Reboot.ps1" -Destination $dest -Force  
+            #Copy-Item "$($outputFolder)\Post-APV2-Reboot-Notification.ps1" -Destination $dest -Force  
+            #copy oobe.xml
+            #$dest = "$($env:windir)\System32\Oobe\Info"
+            #if (-not (Test-Path $dest))
+            #{
+            #    mkdir $dest
+            #}
+            #Copy-Item "$($outputFolder)\oobe.xml" -Destination $dest -Force  
+            if(-not $F)
+            {
+                #provision computer
+                powershell.exe -executionpolicy bypass -file "$($outputFolder)\registerDevice.ps1"
+                powershell.exe -executionpolicy bypass -file "$($outputFolder)\pro2ent.ps1"
+                powershell.exe -executionpolicy bypass -file "$($outputFolder)\add2apv2.ps1"
+                powershell.exe -executionpolicy bypass -file "$($outputFolder)\drivemappingscheduler.ps1"            
+                powershell.exe -executionpolicy bypass -file "$($outputFolder)\addBackgrounds.ps1"
+                powershell.exe -executionpolicy bypass -file "$($outputFolder)\settings.ps1"
+                #powershell.exe -executionpolicy bypass -file "$($outputFolder)\renamePC.ps1"
+    
+                #write installed tag
+                # Create a tag file just so Intune knows this was installed
+                if (-not (Test-Path "$($env:ProgramData)\LeeCounty\PreProvision"))
+                {
+                    Mkdir "$($env:ProgramData)\LeeCounty\PreProvision"
+                }
+                Set-Content -Path "$($env:ProgramData)\LeeCounty\PreProvision\PreProvision.tag" -Value "Installed"
+                
+                Write-Host "*******************************************************************************************" -ForegroundColor Cyan
+                Write-Host "* Pre-Provisioning Complete. Close this window to continue provisioning this device." -ForegroundColor Cyan
+                Write-Host "*******************************************************************************************" -ForegroundColor Cyan
+    
+                #exit 1641
+                #$title    = 'Restart Computer'
+                #$question = 'Do you want to restart this computer now (recommended)?'
+                #$title    = 'Provision Computer'
+                #$question = 'Do you want to continue provisioning this computer?'
+                #$choices  = '&Yes', '&No', '&Repeat the Question'
+                #do {
+                #    $decisionR = $Host.UI.PromptForChoice($title, $question, $choices, 2)
+                #} while ($decisionR -ne 0 -and $decisionR -ne 1)
+                #if ($decisionR -eq 0) {    
+                    #powershell.exe -executionpolicy bypass -file "$($outputFolder)\sysprep.ps1"
+                    #shutdown /g /f /t 0
+                    #exit 1641
+                    #exit
+                #}
+            }
         }
     
     }else {
